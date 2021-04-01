7e18dc858a379662623c62d7595ecd19ddcc8213
Culloo Avinesh <77803431+avinesh1906@users.noreply.github.com>
2021-04-01 17:03:54 +0400

    Add files via upload

---
+++ execution.sql
@@ -0,0 +1,117 @@
+CREATE TABLE Categories (
+categoriesID  int primary key auto_increment ,
+Name varchar(50)
+);
+
+CREATE TABLE Products (
+ productID	int  primary key auto_increment,
+ Name varchar(50)
+ );
+
+ CREATE TABLE food (
+FoodID int primary key auto_increment,
+categoriesID int,
+productID int,
+price int,
+qty int,
+ CONSTRAINT Fk_Categories FOREIGN KEY (categoriesID) REFERENCES Categories(categoriesID),
+ CONSTRAINT Fk_Products FOREIGN KEY (productID) REFERENCES Products(productID)
+ );
+
+ CREATE TABLE Platters (
+ plattersID int primary key ,
+ categoriesID int,
+ platterName VARCHAR(50),
+ price INT,
+ qty int,
+ CONSTRAINT Fk_Category FOREIGN KEY (categoriesID) REFERENCES Categories(categoriesID)
+ );
+
+INSERT INTO Categories (Name) VALUES ('Maki' );
+INSERT INTO Categories (Name) VALUES ('Nigiri' );
+INSERT INTO Categories (Name) VALUES ('Gunkans');
+INSERT INTO Categories (Name) VALUES ('Flower Gunkans' );
+INSERT INTO Categories (Name) VALUES ('California Rolls' );
+INSERT INTO Categories (Name) VALUES ('Rainbow Rolls' );
+INSERT INTO Categories (Name) VALUES ('Futomaki');
+INSERT INTO Categories (Name) VALUES ('Sashimi');
+INSERT INTO Categories (Name) VALUES ('Sushi Platters' );
+
+
+INSERT INTO Products (Name) VALUES ('Cucumbeer');
+INSERT INTO Products (Name) VALUES ('Avocado');
+INSERT INTO Products (Name) VALUES ('Prawn');
+INSERT INTO Products (Name) VALUES ('Salmon');
+INSERT INTO Products (Name) VALUES ('Tuna');
+INSERT INTO Products (Name) VALUES ('Tomato');
+INSERT INTO Products (Name) VALUES ('Zucchini');
+INSERT INTO Products (Name) VALUES ('Panko Prawn');
+INSERT INTO Products (Name) VALUES ('Calamari');
+INSERT INTO Products (Name) VALUES ('Wasabi Prawn');
+INSERT INTO Products (Name) VALUES ('Tuna Tulips');
+INSERT INTO Products (Name) VALUES ('Spicy Salmon Roses');
+INSERT INTO Products (Name) VALUES ('Salmon Roses');
+INSERT INTO Products (Name) VALUES ('Crunchy Athena');
+INSERT INTO Products (Name) VALUES ('Coriander Bomb');
+INSERT INTO Products (Name) VALUES ('Flamed Salom');
+INSERT INTO Products (Name) VALUES ('Fried Prawn');
+INSERT INTO Products (Name) VALUES ('Tempura Rock Shrimp');
+INSERT INTO Products (Name) VALUES ('Lemon Salmon');
+INSERT INTO Products (Name) VALUES ('Salom and Avo');
+INSERT INTO Products (Name) VALUES ('Tuna and Avo');
+INSERT INTO Products (Name) VALUES ('Sweet Prawn');
+INSERT INTO Products (Name) VALUES ('Panko Salmon');
+INSERT INTO Products (Name) VALUES ('Avocado');
+
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (1,1,135,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (1,2,145,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (1,3,235,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (1,4,255,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (1,5,235,50);
+
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (2,6,135,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (2,7,135,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (2,3,155,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (2,4,165,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (2,5,155,50);
+
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (3,8,205,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (3,9,215,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (3,10,205,50);
+
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (4,11,185,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (4,12,245,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (4,13,245,50);
+
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (5,3,145,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (5,4,155,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (5,5,145,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (5,14,265,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (5,15,155,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (5,16,165,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (5,17,145,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (5,18,155,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (5,19,165,50);
+
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (6,20,250,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (6,21,225,50);
+
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (7,22,275,50);
+INSERT INTO food (categoriesID, productID, price, qty) VALUES (7,23,295,50);
+
+INSERT INTO Platters (plattersID,categoriesID, platterName, price, qty) VALUES (30,8,'3 pieces', 285,50);
+INSERT INTO Platters (plattersID,categoriesID, platterName, price, qty) VALUES (31,8,'6 pieces', 525,50);
+INSERT INTO Platters (plattersID,categoriesID, platterName, price, qty) VALUES (32,8,'9 pieces', 695,50);
+
+INSERT INTO Platters (plattersID,categoriesID, platterName, price, qty) VALUES (33,9,'Fusion Crunch Platter', 745,50);
+INSERT INTO Platters (plattersID,categoriesID, platterName, price, qty) VALUES (34,9,'Flower Power Platter', 455,50);
+INSERT INTO Platters (plattersID,categoriesID, platterName, price, qty) VALUES (35,9,'Salmon Platter', 1175,50);
+INSERT INTO Platters (plattersID,categoriesID, platterName, price, qty) VALUES (36,9,'Zen Platter', 755,50);
+INSERT INTO Platters (plattersID,categoriesID, platterName, price, qty) VALUES (37,9,'Rising Sun Platter', 625,50);
+INSERT INTO Platters (plattersID,categoriesID, platterName, price, qty) VALUES (38,9,'Bonsai Platter (Veg)', 455,50);
+INSERT INTO Platters (plattersID,categoriesID, platterName, price, qty) VALUES (39,9,'Sushi for 1', 525,50);
+INSERT INTO Platters (plattersID,categoriesID, platterName, price, qty) VALUES (40,9,'Sushi for 2', 945,50);
+
+
+
+
---
+++ server.py
@@ -0,0 +1,125 @@
+# import libraries
+import mysql.connector
+import socket
+import pickle
+from _thread import *
+
+HOST = '127.0.0.1'  # <-- insert the HOST of the database
+PORT = 3306  # <-- insert the PORT of the database
+
+mydb = mysql.connector.connect(
+    user='root',  # <-- insert the username
+    password='Cm3tc7At',  # <-- insert the user password
+    host=HOST,
+    database='sushi',  # <-- insert the database name here
+    port=PORT,
+    raise_on_warnings=True,  # whether warnings should raise exceptions.
+)
+
+
+# function to keep the connection active
+def connection_Thread(client_Socket):
+    # Cursor is used to execute statements to communicate with the MYSQL database
+    # buffered=True enable to execute different queries
+    database = mydb.cursor(buffered=True)
+
+    # exception handling
+    try:
+        # infinite loop to prevent the connection from closing
+        while True:
+            # Retrieve the categories name from MyDb
+            database.execute("SELECT * FROM categories ")
+            categories_data = database.fetchall()
+
+            # Retrieve the categories name from MyDb
+            database.execute("SELECT * FROM food ")
+            food_data = database.fetchall()
+
+            # Retrieve the categories name from MyDb
+            database.execute("SELECT * FROM platters ")
+            platter_data = database.fetchall()
+
+            # Retrieve the categories name from MyDb
+            database.execute("SELECT * FROM products ")
+            products_data = database.fetchall()
+
+            database_list = [categories_data, food_data, platter_data, products_data]
+            # Send data to the client
+            # pickle will allow to send a list to the client
+            server_message = pickle.dumps(database_list)
+            client_Socket.send(server_message)
+
+            # receive the order.csv from the client
+            order_details = pickle.loads(client_Socket.recv(1024))
+
+            for orders in order_details:
+                # check whether to extract from food or platters according to categoryID
+                if int(orders[3]) <= 29:
+                    # sql to retrieve the qty from food table
+                    currentStockSQL = """SELECT qty FROM food WHERE FoodID = %s"""
+                    # tuple to store the categoryID
+                    stockLevel_Tuple = (orders[3],)
+                    # execute the query
+                    database.execute(currentStockSQL, stockLevel_Tuple)
+
+                    for row in database:
+                        # calculate the new stock level
+                        newStock = row[0] - int(orders[1])
+                        # sql to update food table
+                        stockControl_SQL = " UPDATE food SET qty = %s WHERE FoodID = %s"
+
+                else:
+                    # sql to retrieve the qty from platters table
+                    currentStockSQL = """SELECT qty FROM platters WHERE plattersID = %s"""
+                    # tuple to store the categoryID
+                    stockLevel_Tuple = (orders[3],)
+                    # execute the query
+                    database.execute(currentStockSQL, stockLevel_Tuple)
+                    for row in database:
+                        # calculate the new stock level
+                        newStock = row[0] - int(orders[1])
+                        # sql to update platter table
+                        stockControl_SQL = " UPDATE platters SET qty = %s WHERE plattersID = %s"
+                # execute the sql to update the qty
+                order_Tuple = (newStock, orders[3])
+                database.execute(stockControl_SQL, order_Tuple)
+                mydb.commit()
+
+    except socket.error as err:
+        # print the error occured
+        print(f"Error occurred: {err} ")
+        # close the socket
+        client_Socket.close()
+
+
+def main():
+    # Create a server socket
+    # AF.INET refers to the address family IPV4
+    # SOCK_STREAM means connection oriented TCP protocol
+    try:
+        # start the connection
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            print("Socket successfully created")
+            # Bind
+            s.bind(('localhost', 69))
+
+            # listen
+            s.listen(10)
+            print("Waiting for connection")
+
+            # forever loop until client close the application
+            while True:
+                # Accept the connection
+                client_Socket, address = s.accept()
+                print('Connected to :', address[0], ':', address[1])
+
+                # Start a new thread to keep the connection
+                start_new_thread(connection_Thread, (client_Socket,))
+    # print any error if occured
+    except socket.error as err:
+        print(f"{err} occurred during socket creation")
+        s.close()
+
+
+if __name__ == '__main__':
+    main()
